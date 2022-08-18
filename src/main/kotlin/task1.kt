424c834db53e11f36fbcea33de85460476ce820d
boudyK <abdalrahmank2000@gmail.com>
2022-08-18 18:48:34 +0200

    data structure

--- src/main/kotlin/task1.kt
+++ src/main/kotlin/task1.kt
@@ -1,8 +1,30 @@
-fun main(){
+import java.util.Scanner
+import kotlin.math.ln
 
-    var x=20
-    var y=30
-    var sum=x+y
-    println(sum)
+fun main(){
+    println("enter the size ")
+    val n  =Scanner(System.`in`).nextInt()
+    var counter =0
+    val my_list=ArrayList<String>()
+    while (counter < n)
+    {
+        println("enter the input ")
+        val input=Scanner(System.`in`).nextLine()
+        my_list.add(input)
+        counter++
+    }
+
+
+    for (item in my_list)
+    {
+      if(item.contains('b',true)|| item.contains('m',true)){
+          println(item.uppercase())
+      }
+    }
+
 
 }
+
+
+
+
